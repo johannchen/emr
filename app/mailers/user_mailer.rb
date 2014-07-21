324cd04a4980ada446837ba1f52697e011f8c86c
johannchen <johannchen@gmail.com>
2014-07-21 10:59:33 +0800

    update domain name in mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url = 'http://localhost:3000'
+    @url = 'https://lightemr.herokuapp.com'
     mail(to: @user.email, subject: 'Welcome to Light EMR')
   end
 end
